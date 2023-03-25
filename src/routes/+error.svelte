7f1fd21463e5c869df9e1db645cf35a5cfac0013
HarryXu497 <harryxu497@gmail.com>
2023-03-25 19:34:23 -0400

    added rudimentary error handling

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,3 +1,16 @@
-<div>
-	<p>+ERROR.SVELTE</p>
-</div>
+<main>
+	<p>Sorry, There Was An Error</p>
+</main>
+
+<style lang="scss">
+	@use "./../styles/exports.scss" as exports;
+
+	main {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		font-family: exports.$font-sans-serif;
+		font-size: 6rem;
+		height: calc(100vh - 10rem);
+	}
+</style>
