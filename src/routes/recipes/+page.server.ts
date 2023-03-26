a73c38b2ec028b0ae3deb9f3375fa657a32dc0c3
HarryXu497 <harryxu497@gmail.com>
2023-03-25 21:16:17 -0400

    removed unnecessary requests

--- src/routes/recipes/+page.server.ts
+++ src/routes/recipes/+page.server.ts
@@ -8,16 +8,13 @@ import type { PageServerLoad } from './$types';
 export const load = (async () => {
 	const recipesCollection = pb.collection("recipes");
 	const recipes = await recipesCollection.getList<Recipe>(1, 40, {
-		expand: "author,ingredients",
+		expand: "",
 	})
 
 	return {
 		recipes: recipes.items.map(o => ({
 			...o,
 			images: o.images.map((url: string) => pb.getFileUrl(o, url)),
-			expand: {
-				ingredients: o.expand.ingredients.map((o: Record) => ({...o})) as Ingredient[],
-			}
 		}))
 	};
 }) satisfies PageServerLoad;
